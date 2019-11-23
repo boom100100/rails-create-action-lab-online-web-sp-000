fa4bb49847d2f1dedb7eef0a4a038621347f969a
Bernadette Davis <bsd231@nyu.edu>
2019-11-23 22:53:26 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,9 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
   end
 
   def create
