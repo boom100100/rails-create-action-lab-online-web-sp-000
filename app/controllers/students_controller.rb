2e818f61bb91892aa3ad65c47e1754be3a58301a
Bernadette Davis <bsd231@nyu.edu>
2019-11-23 23:03:15 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,11 +10,7 @@ class StudentsController < ApplicationController
 
   def new
     @student = Student.new
-    @student.first_name = params[:student][:first_name]
-    @student.last_name = params[:student][:last_name]
-    @student.save
     
-    redirect_to student_path(@student)
   end
 
   def create
